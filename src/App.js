7a3f340becca40ac0ad4d45295c0598fb958336e
frandzo <frandzo@users.noreply.github.com>
2021-12-20 23:37:12 -0300

    creados los componentes header y producto, llamados a App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Header} from './Header.js';
+import {Product} from './Product.js';
+import json from "./products.json";
 
 function App() {
+  // json.products[1].title
+
+  let productos=json.products.map((obj)=>{return <Product item={obj}></Product>})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header></Header>
+        {/* <Product item={json.products[1]}></Product> */}
+        {productos};
     </div>
   );
 }
