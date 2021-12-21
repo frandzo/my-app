d7c834f0ae8e6f158ce36964ab2e06d6e58f122d
Franco Direnzo <fm.direnzo@gmail.com>
2021-12-21 14:27:12 -0300

    update css and app.js structure

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+export function Product(props){
+    let title = props.item.title;
+    let img = props.item.image;
+    let price = props.item.price;
+        return (<div className="Product">
+            <h4>{title}</h4>
+            <img src={img}/>
+            <h2>${price}</h2>
+        </div>)
+}
