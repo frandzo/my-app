664518733ea52b95a16200b61b3da6db1ddd9a38
Franco Direnzo <fm.direnzo@gmail.com>
2021-12-30 11:37:04 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,19 @@
+require('bootstrap');
+
 export function Header(){
-    return (<h1>MI TIENDA ONLINE</h1>)
+    return (
+    <div>
+        <h1>MI TIENDA ONLINE</h1>
+        <select name="cars" id="cars">
+        <optgroup label="Swedish Cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+        </optgroup>
+        <optgroup label="German Cars">
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+        </optgroup>
+        </select>
+    </div>
+    )
 }
